24755fd2c7b3ca8ca3951e451e7b768806d71a4f
Гладков Максим Юрьевич <mgladkov@ABZ-1.ru>
2019-06-17 09:57:26 +0300

    финиш

--- js/js/gm.js
+++ js/js/gm.js
@@ -1,103 +1,82 @@
 'use strict';
 
-Date.prototype.format = function (mask, utc) {
-	return this.toISOString().split('T')[0];
-};
-
-Date.prototype.firstDayOfMonth = function() {
-    let y = this.getFullYear()
-      , m = this.getMonth()
-      , d = new Date(y, m, 2);
-
-    d.setUTCHours(0);
-    return d;
-}
-
-Date.prototype.lastDayOfMonth = function() {
-    let y = this.getFullYear()
-      , m = this.getMonth()
-      , d = new Date(y, m + 1, 1);
-
-    d.setUTCHours(0);
-    return d;
-}
-
-Date.prototype.startOfDay = function() {
-    return new Date(new Date().setUTCHours(0, 0, 0, 1));
-}
-
-Date.prototype.endOfDay = function() {
-    return new Date(new Date().setUTCHours(23, 59, 59, 999));
-}
-
 let gm = (function(){
-    function Binding(object, property) {
-        let self = this;
-
-        this.elementBindings = [];
-        this.value           = object[property];
-
-        this._safeSet = function(value, description) {
-            let result = value;
-            if(value instanceof Date) {
-                result = value.format(description.format, true);
-            }
-
-            return result;
-        }
-
-        this.getter = function(){
-            return self.value;
-        }
-
-        this.setter = function(value){
-            self.value = value;
-            for (var i = 0; i < self.elementBindings.length; i++) {
-                let binding = self.elementBindings[i];
-                binding.element[binding.attribute] = self._safeSet(self.value, binding);
-            }
-        }
-
-        this.addBinding = function(binding) {
-            switch(binding.event) {
-                case 'click': 
-                    binding.element.addEventListener(binding.event, self.value); break;
-                default: 
-                    binding.element.addEventListener(binding.event, function(event){
-                        let value = binding.element[binding.attribute];
-                        if(binding.type === 'date') {
-                            value = new Date(value);
-                        }
-                        self.setter(value);
-                    });
-            }
-
-            self.elementBindings.push(binding);
-            if(binding.attribute !== '')
-                binding.element[binding.attribute] = self._safeSet(self.value, binding);
+    let signals = {};
+    /*
+    Отслеживание зависимостей. Идея в следующем:
+    1. Вызывается вычисляемое свойство (вызов get). Устанавливается контекст. 
+    2. За вызовом вычисляемого свойства, следуют вызовы (get) наблюдаемых свойств, от которых зависит вычисляемое свойство.
     
-            return self;
-        }
+    З.Ы. 
+    computed - вычисляемое свойство.
+    observable - наблюдаемое свойство.
+    */
+    let Dep = {
+        target: null, // контекст вычисляемого значения
+        subs: {},     // наблюдаемые свойства, от которых зависит вычисляемое значение
+        depend (deps, dep) {
+            // Если контекст еще не сохранен - сохранить его
+            if (!deps.includes(this.target)) {
+                deps.push(this.target);
+            }
 
-        if(typeof object[property] !== 'function') {
-            Object.defineProperty(object, property, {
-                get: self.getter,
-                set: self.setter
-            }); 
+            // Добавить наблюдаемые свойства в контекст вычисляемого значения
+            if (!Dep.subs[this.target].includes(dep)) {
+                Dep.subs[this.target].push(dep);
+            }
+        },
+        getValidDeps (deps, key) {
+            // Поддержка зависимостей в актуальном состоянии
+            return deps.filter(dep => this.subs[dep].includes(key));
+        },
+        notifyDeps (deps) {
+            // Уведомить всех об изменении
+            deps.forEach(notify);
         }
-
-        if(typeof self.value !== 'function')
-            object[property] = self.value;
     }
 
-    function addServiceProperty(object) {
-        Object.defineProperty(object, '__bindings__', {
-            value: [],
-            enumerable: true
+    Date.prototype.format = function (mask, utc) {
+        return this.toISOString().split('T')[0];
+    };
+
+    // Внешний интерфейс библиотеки
+    return {
+        applyBindings: (model, view = document) => {
+            buildModel(model);
+            buildBindings(model, view);
+        },
+        subscribe: (property, fn) => {
+            observe(property, fn);
+        }
+    }
+
+    function buildModel(model) {
+        for(let key in model) {
+            if(typeof model[key] === 'function')
+                computed(model, key, model[key]);
+            else 
+                observable(model, key);
+        }
+    }
+
+    function buildBindings(model, view) {
+        let elements = view.querySelectorAll('[data-bind]');
+        elements.forEach(element => {
+            let description = bindDescription(model, element, element.dataset.bind);
+            if(description !== undefined) {
+                element.addEventListener(description.event, (event) => {
+                    let value = description.element[description.attribute];
+                    if(description.element.type === 'date') {
+                        value = new Date(value);
+                    }
+                    model[description.property] = value;
+                });
+            }
+            updateUI(model, description);
         });
     }
 
-    function buildBindDescription(object, element, b) {
+    function bindDescription(object, element, b) {
         let description = {
             element: '',
             attribute: '',
@@ -109,8 +88,8 @@ let gm = (function(){
 
         if(b === undefined) 
             return undefined;
-        
-        let bvals = b.split(',');
+
+            let bvals = b.split(',');
         for(let i = 0; i < bvals.length; i++) {
             let propval = bvals[i].split(':')
               , prop    = propval[0].trim()
@@ -128,19 +107,82 @@ let gm = (function(){
         return description;
     }
 
-    function applyBindings(object, view = document) {
-        let elements = view.querySelectorAll('[data-bind]');
-        addServiceProperty(object);
-        elements.forEach((element) => {
-            let description = buildBindDescription(object, element, element.dataset.bind);
-            if(description !== undefined) {
-                object.__bindings__[description.property] = new Binding(object, description.property);
-                object.__bindings__[description.property].addBinding(description);
+    // Объявление и реализация наблюдаемого свойства
+    function observable(object, key) {
+        let deps = []            // массив зависимостей
+          , value = object[key]; // текущее значение свойства
+
+        Object.defineProperty(object, key, {
+            get: function() {
+                // Если находится в контексте вычисляемого значения, добавить себя в зависимости
+                if (Dep.target) {
+                    Dep.depend(deps, key)
+                }
+
+                return value;
+            },
+            set: function(newVal) {
+                value = newVal;
+
+                // При установке нового значения - обновить зависимости
+                deps = Dep.getValidDeps(deps, key);
+                Dep.notifyDeps(deps, key);
+
+                // Уведомить всех наблюдателей
+                notify(key);
             }
         });
     }
 
-    return {
-        applyBindings: applyBindings
+    function computed(object, key, fn) {
+        let deps  = [];
+
+        Object.defineProperty(object, key, {
+            get: function() {
+                // Добавить себя в зависимости, если нахожусь в контексте другого вычисляемого свойства
+                if (Dep.target) {
+                    Dep.depend(deps, key);
+                }
+
+                // Переключить контекст на себя
+                Dep.target    = key;
+                Dep.subs[key] = [];
+                let value     = fn.call(object);
+  
+                // очистить контекст
+                Dep.target = null;
+                return value;
+            },
+            set: function() {} // Вычисляемому свойству set не нужен, поэтому пустая функция.
+        });
+    }
+
+    // часть паттерна observer, хранит сохраняет обработчики к наблюдаемым свойствам
+    function observe (property, signalHandler) {
+        if(!signals[property]) 
+            signals[property] = [];
+        signals[property].push(signalHandler);
+    }
+
+    // часть паттерна observer, уведомляет наблюдателей об изменениях
+    function notify(signal) {
+        if(!signals[signal] || signals[signal].length < 1) 
+            return;
+        signals[signal].forEach(signalHandler => signalHandler());
+    }
+
+    // обновляет пользовательские элементы управления
+    function updateUI(model, description) {
+        description.element[description.attribute] = safeSet(model[description.property], description);
+        observe(description.property, () => description.element[description.attribute] = safeSet(model[description.property], description));
+    }
+
+    function safeSet(value, description) {
+        let result = value;
+        if(value instanceof Date) {
+            result = value.format(description.format, true);
+        }
+
+        return result;
     }
 }());
--- js/js/index.js
+++ js/js/index.js
@@ -3,13 +3,22 @@
 let vm = {
     firstName: 'Максим',
     lastName: 'Гладков',
-    birthday: new Date('1979-05-04')
+    birthday: new Date('1979-04-05')
 }
 
 vm.fullName = function() { 
-    return this.firstName + ' ' + this.lastName; 
+    return this.firstName + ' ' + this.lastName + ', год рождения: ' + this.birthday.getUTCFullYear(); 
 }.bind(vm);
 
 document.addEventListener('DOMContentLoaded', function() {
     gm.applyBindings(vm);
+    gm.subscribe('firstName', () => {
+        console.log(vm.firstName);
+    });
+    gm.subscribe('lastName', () => {
+        console.log(vm.lastName);
+    });
+    gm.subscribe('birthday', () => {
+        console.log(vm.birthday);
+    });
 }, false);
